970ce6ca575d931cea36f77930a841872cad650a
lxxhack <1572411660@qq.com>
2017-07-10 17:36:46 +0800

    Update main.js

--- pos_v0/main/main.js
+++ pos_v0/main/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
 function printReceipt(inputs) {
-  console.log('请在此实现练习要求，并改写该行代码。');
+var str='***<没钱赚商店>收据***\n';
+var sum=0;
+for (var i=0;i<inputs.length;i++)
+{
+var count=inputs[i].price*inputs[i].count;
+sum+=count;
+str+=`名称：${inputs[i].name}，数量：${inputs[i].count}${inputs[i].unit}，单价：${inputs[i].price.toFixed(2)}(元)，小计：${count.toFixed(2)}(元)\n`;
+}
+str+=`----------------------
+总计：${sum.toFixed(2)}(元)
+**********************`;
+console.log(str);
 }
