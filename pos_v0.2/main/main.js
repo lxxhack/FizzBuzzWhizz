85030a685fe107e9099072b0eebccf3e95dad781
lxxhack <1572411660@qq.com>
2017-07-11 14:59:11 +0800

    Update main.js

--- pos_v0.2/main/main.js
+++ pos_v0.2/main/main.js
@@ -1,5 +1,55 @@
 'use strict';
+function buildSheet(inputs)
+{
+var goods=loadAllItems();
+var outputs=[];
+for (var i=0;i<inputs.length;i++)
+{
+for (var j=0;j<goods.length;j++)
+{
+if (inputs[i]==goods[j].barcode)
+{
+outputs[i]=goods[j];
+break;
+}
+}
+}
+return outputs;
+}
+
+function statistics(inputs)
+{
+var outputs=[];
+var i=0,j=0;
+do 
+{
+outputs[j]=inputs[i];
+outputs[j].count=1;
+i++
+while (i<inputs.length&&outputs[j].barcode==inputs[i].barcode)
+{
+outputs[j].count++;
+i++;
+}
+j++;
+}
+while(i<inputs.length);
+return outputs;
+}
 
 function printReceipt(inputs) {
-  console.log('请在此实现练习要求，并改写该行代码。');
+inputs=buildSheet(inputs);
+inputs=statistics(inputs);
+var sum=0;
+var str='***<没钱赚商店>收据***\n';
+for (var i=0;i<inputs.length;i++)
+{
+var count=inputs[i].price*inputs[i].count;
+sum+=count;
+str+=`名称：${inputs[i].name}，数量：${inputs[i].count}${inputs[i].unit}，单价：${inputs[i].price.toFixed(2)}(元)，小计：${count.toFixed(2)}(元)\n`;
+}
+str+=`----------------------
+总计：${sum.toFixed(2)}(元)
+**********************`;
+console.log(str);
 }
