7b28d220ae496bbad330a2f572d7da9b31ef7305
lxxhack <1572411660@qq.com>
2017-07-17 07:49:41 +0800

    Update fizzBuzzWhizz.js

--- fizzBuzzWhizz.js
+++ fizzBuzzWhizz.js
@@ -9,3 +9,15 @@ function printText(num) {
     text=num%c==0?text+'Whizz':text;
     return text;
 }
+function printText2(num,base,word) {
+    let text = '';
+    const a = base[0], b = base[1], c = base[2];
+    if (num.toString().indexOf(a.toString())!=-1)
+    {
+      return word[0];
+    }
+    text=num%a==0?text+word[0]:text;
+    text=num%b==0?text+word[1]:text;
+    text=num%c==0?text+word[2]:text;
+    return text;
+}
